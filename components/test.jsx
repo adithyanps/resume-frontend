56f23b1b528bd7896407bf42d60407a76a1661fe
adithyanps <adithyanps3@gmail.com>
2021-05-15 12:57:57 +0530

    list test

---
+++ components/test.jsx
@@ -0,0 +1,16 @@
+import React, {useEffect} from 'react'
+
+export const test = () => {
+  const a =[3],
+  useEffect(() => {
+    effect
+    return () => {
+      console.log(a)
+    }
+  }, [])
+  return (
+    <div>
+test
+    </div>
+  )
+}
